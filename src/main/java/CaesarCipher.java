81a01d911eef0c8b9fd4ef746c6f4f96ec9b7fe9
mervinkogei <vincent.kogei@yahoo.com>
2019-11-08 14:41:08 +0300

    add the a test of shouldNotCipherSymbols

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,2 +1,6 @@
 public class CaesarCipher {
+//    private static  final int ALPHABET_SIZE=26;
+    public String cipher(String symbols, int i) {
+        return symbols;
+    }
 }
--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -1,2 +1,15 @@
+import org.junit.*;
+import org.junit.Assert.*;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class CaesarCipherTest {
+    private CaesarCipher caesar = new CaesarCipher();
+
+    @Test
+    public void shouldNotCipherSymbols(){
+//        assertEquals("-",caesar.cipher("-",3));
+        String symbols="1!@#$%^&*(){}[]/";
+                assertEquals(symbols,caesar.cipher(symbols,3));
+    }
 }
