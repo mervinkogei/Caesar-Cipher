4716e276c267156eee97983f0b2c572ad2fe2238
mervinkogei <vincent.kogei@yahoo.com>
2019-11-11 07:40:22 +0300

    add the encryption function to convert strings to uppercase and to determine the number of shift a letter should move

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -7,25 +7,37 @@ public class CaesarCipherTest {
     private CaesarCipher caesar = new CaesarCipher();
 
     @Test
-    public void shouldNotCipherSymbols(){
+    public void shouldNotCipherSymbols() {
 //        assertEquals("-",caesar.cipher("-",3));
-        String symbols="1!@#$%^&*(){}[]/";
-                assertEquals(symbols,caesar.cipher(symbols,3));
+        String symbols = "1!@#$%^&*(){}[]/";
+        assertEquals(symbols, caesar.cipher(symbols, 3));
     }
 
     @Test
-    public void shouldNotBeEmptyString(){
-        assertEquals("",caesar.cipher("",3));
+    public void shouldNotBeEmptyString() {
+        assertEquals("", caesar.cipher("", 3));
     }
 
     @Test
-    public void shouldCipherUpperCaseLetter(){
-        assertEquals("A",caesar.cipher("A",0));
+    public void shouldCipherUpperCaseLetter() {
+        assertEquals("A", caesar.cipher("A", 0));
     }
 
-//    @Test
-//    public void shouldCipherWholeAlphabet(){
-//        String allChars = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
-//        assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD",caesar.cipher(allChars,-3));
-//    }
+    @Test
+    public void shouldDecipher() {
+        String s = "aoeu";
+
+        String encrypted = caesar.cipher(s, 2);
+
+        assertEquals(s, caesar.cipher(encrypted, -2));
+    }
+
+
+    @Test
+    public void shouldCipherWholeAlphabet() {
+        String allChars = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD",
+                caesar.cipher(allChars, -3));
+    }
 }
+
